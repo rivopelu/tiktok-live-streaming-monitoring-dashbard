d8f0a0b15f7915c1caaf87cfc32221f644fe05c0
rivopelu <rivopelu12@gmail.com>
2025-02-20 03:24:25 +0700

    FEAT:fiig

--- src/components/molecules/TopBar.tsx
+++ src/components/molecules/TopBar.tsx
@@ -1,17 +1,37 @@
 import { MdPerson } from 'react-icons/md';
 import { IconButton } from '../atoms/IconButton.tsx';
 import { StreamingStatusText } from '../atoms/StreamingStatusText.tsx';
+import { HttpService } from '../../services/http.service.ts';
+import { useEffect, useState } from 'react';
+import { ENDPOINT } from '../../constants/endpoint.ts';
+import { BaseResponse } from '../../models/response/IResModel.ts';
+
+type typeActiveStreaming = 'ACTIVE' | 'INACTIVE' | undefined;
 
 export function TopBar() {
+  const httpService = new HttpService();
+
+  const [activeStreaming, setActiveStreaming] = useState<typeActiveStreaming>();
+
+  useEffect(() => {
+    httpService.GET(ENDPOINT.CHECK_STREAMING_STATUS()).then((res: BaseResponse<boolean>) => {
+      setActiveStreaming(res.data ? 'ACTIVE' : 'INACTIVE');
+    });
+  }, []);
+
   return (
     <div className={'fixed z-[99] border-b-2 w-screen h-top-bar-height bg-white'}>
       <div className={'w-full  pl-4 px-10 h-full flex items-center justify-between'}>
         <h1>TOP BAR</h1>
         <div className={'flex gap-7'}>
-          <StreamingStatusText />
-          <div>
-            <div className={'h-full bg-slate-200 w-1 rounded-full '}></div>
-          </div>
+          {activeStreaming && (
+            <>
+              <StreamingStatusText />
+              <div>
+                <div className={'h-full bg-slate-200 w-1 rounded-full '}></div>
+              </div>
+            </>
+          )}
           <IconButton>
             <MdPerson />
           </IconButton>
--- src/constants/endpoint.ts
+++ src/constants/endpoint.ts
@@ -2,4 +2,5 @@ export const ENDPOINT = {
   SIGN_IN: () => `/auth/v1/admin/sign-in`,
   START_STREAMING: () => `/streaming/v1/start-streaming`,
   END_STREAMING: () => `/streaming/v1/end-streaming`,
+  CHECK_STREAMING_STATUS: () => `/account/v1/check-streaming-status`,
 };
